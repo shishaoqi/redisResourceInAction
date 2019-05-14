185016451a03e2d370c60e46979176eb9d695d6c
shishao-home <shishaoqi0928@gmail.com>
2019-05-15 01:24:30 +0800

    注释修改

--- 01-SDS/test-sds/demo_1.c
+++ 01-SDS/test-sds/demo_1.c
@@ -2,7 +2,7 @@
 * @Author: shishao
 * @Date:   2018-10-28 23:08:37
 * @Last Modified by:   shishao
-* @Last Modified time: 2018-11-04 22:47:44
+* @Last Modified time: 2019-05-14 23:51:47
 * @Describtion: sdsnew sdslen  sdsnewlen functions test.
 */
 
@@ -14,15 +14,18 @@ int main() {
     char *str = "hello";
     sds sdsStr;
 
+    // 创建一个包含给定C字符串的SDS
     sdsStr = sdsnew(str);
     printf("sdsStr content: %s\n", sdsStr);
 
+    // 返回SDS的已使用空间字节数
     size_t length = sdslen(sdsStr);
     printf("sdsStr length: %d\n", length);
 
     sds secondStr = "keep doing";
+    // 根据给定的初始化字符串 init 和字符串长度 initlen，创建一个新的 sds
     sdsStr = sdsnewlen(secondStr, strlen(secondStr));
-    printf("new sdsStr content: %s\n", secondStr);
+    printf("new sdsStr content: %s\n", sdsStr);
 
     length = sdslen(sdsStr);
     printf("new sdsStr length: %d\n", length);
